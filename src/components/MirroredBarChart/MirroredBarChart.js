572a5b27c2ceb0f111c5208d1225e5267a9b4cb0
Joseph Reid <joseph.james.reid@gmail.com>
2020-02-01 12:49:02 -0800

    empty component files and directory structure

---
+++ src/components/MirroredBarChart/MirroredBarChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './MirroredBarChart.css';
+
+class MirroredBarChart extends Component {
+  render() {
+
+    return (
+
+    );
+  }
+}
+
+export default MirroredBarChart;
---
+++ src/components/Select/Select.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Select.css';
+
+class Select extends Component {
+  render() {
+
+    return (
+
+    );
+  }
+}
+
+export default Select;
