9d7c8d24144845deddbb20791699ebe02874978a
Joseph Reid <joseph.james.reid@gmail.com>
2020-02-07 21:38:05 -0800

    Add key value to map fcn in Select component.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,8 +7,9 @@ class Select extends Component {
     return (
       <div className={`Content-Dataview-Controls-${this.props.name}`} >
         <select onChange={this.props.changeFcn}>
-          {Object.entries(this.props.items).map(([key, value]) => (
-            <option className="Content-Dataview-Controls-Option"
+          {Object.entries(this.props.items).map(([key, value],index) => (
+            <option  key={index}
+                     className="Content-Dataview-Controls-Option"
                      value= {key}> {value} </option>))}
         </select>
       </div>)
